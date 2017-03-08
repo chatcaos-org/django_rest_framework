07cf40f7597d72265bc93ac5d1e1ea07725a790d
Claudio Santos <contato@simplologia.com.br>
2017-03-08 03:09:12 -0300

    start hands on django rest framework

---
+++ src/hdson_rest/hdson_rest/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import include, url
+
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    url(r'^api/token/', obtain_auth_token, name='api-token'),
+]
