399a535414e18488dc968b0935d2872c94fd813e
Claudio Santos <contato@simplologia.com.br>
2017-03-08 08:42:36 -0300

    organização do diretório

--- src/hdson_rest/hdson_rest/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import include, url
-
-from rest_framework.authtoken.views import obtain_auth_token
-
-urlpatterns = [
-    url(r'^api/token/', obtain_auth_token, name='api-token'),
-]
